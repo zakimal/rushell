81b747c70767c9af20fafec03b37eaa0025d2692
zakimal <oza777you@gmail.com>
2019-02-27 00:04:47 +0900

    Use rustyline for reading commandline and parse it

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
+extern crate rustyline;
+
 use std::env;
-use std::io;
-use std::io::Write;
 use std::process::Command;
 
+use rustyline::Editor;
+use rustyline::error::ReadlineError;
+
 fn main() {
+
+    // set reader to read an input line
+    let mut reader = Editor::<()>::new();
+
     // get user name from environment variables
     let user = env::var("USER").unwrap();
 
@@ -16,31 +23,61 @@ fn main() {
         // print prompt
         print!("({}@{})\n(rushell)$ ", user, cwd);
 
-        // flush it to stdout
-        io::stdout().flush().unwrap();
+        let prompt = format!("{}@{}\n(rushell)$ ", user, cwd);
 
-        // read commandline input
-        let mut cmd = String::new();
-        io::stdin().read_line(&mut cmd)
-            .expect("Error: failed to read line");
+        // read a line
+        let cmd = reader.readline(prompt.as_str());
 
-        if cmd.trim() == "exit" {
-            println!("Bye!");
-            break;
-        } else if cmd.trim() == "" {
-            continue;
+        // parse it
+        match cmd {
+            Ok(line) => {
+                if line.trim() == "exit" {
+                    println!("Bye!");
+                    break;
+                } else if line.trim() == "" {
+                    continue;
+                }
+
+                // add the input line as a history
+                reader.add_history_entry(&line);
+
+                // split the line into Vec<&str>
+                let args: Vec<&str> = line.trim().split(' ').collect();
+
+                // execute it
+                match Command::new(args[0])
+                    .args(&(args[1..]))
+                    .output() {
+                    Ok(output) => {
+                        let err = String::from_utf8_lossy(&output.stderr);
+                        if err != "" {
+                            print!("{}", err);
+                        }
+                        let out = String::from_utf8_lossy(&output.stdout);
+                        if out != "" {
+                            print!("{}", out);
+                        }
+                    },
+                    Err(e) => {
+                        println!("{:?}", e);
+                    }
+                }
+            },
+
+            // caught SIGINT(Ctrl-C)
+            Err(ReadlineError::Interrupted) => {
+                continue;
+            },
+
+            // caught EOF(Ctrl-D)
+            Err(ReadlineError::Eof) => {
+                continue;
+            }
+
+            Err(err) => {
+                println!("Reader Error: {:?}", err);
+                continue;
+            }
         }
-
-        // parse the arguments
-        let args: Vec<&str> = cmd.trim().split(' ').collect();
-
-        // execute them
-        let output = Command::new(args[0])
-            .args(&(args[1..]))
-            .output()
-            .expect("Error: failed to run");
-
-        // print the output
-        print!("{}", String::from_utf8(output.stdout).unwrap());
     }
 }
