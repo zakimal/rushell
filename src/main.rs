bb7339ad29786b734c3bbb20c4ca34671f06067f
zakimal <oza777you@gmail.com>
2019-02-27 23:17:55 +0900

    Add color

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 extern crate rustyline;
+extern crate ansi_term;
 
 use std::env;
 use std::process::Command;
 
 use rustyline::Editor;
 use rustyline::error::ReadlineError;
+
+use ansi_term::Colour::Red;
+use ansi_term::Colour::Green;
 
 fn main() {
 
@@ -14,16 +18,28 @@ fn main() {
     // get user name from environment variables
     let user = env::var("USER").unwrap();
 
+    // status flag
+    let mut proc_status_flag = true;
+
     // main loop
     loop {
 
         // get current working directory
         let cwd = env::var("PWD").unwrap();
 
-        // print prompt
-        print!("({}@{})\n(rushell)$ ", user, cwd);
+        let prompt;
 
-        let prompt = format!("{}@{}\n(rushell)$ ", user, cwd);
+        if proc_status_flag {
+            prompt = format!("{}@{}\n(rushell)$ ",
+                             Green.paint(user.to_string()),
+                             Green.paint(cwd.to_string())
+                            )
+        } else {
+            prompt = format!("{}@{}\n(rushell)$ ",
+                             Red.paint(user.to_string()),
+                             Red.paint(cwd.to_string())
+            )
+        }
 
         // read a line
         let cmd = reader.readline(prompt.as_str());
@@ -31,6 +47,7 @@ fn main() {
         // parse it
         match cmd {
             Ok(line) => {
+                proc_status_flag = true;
                 if line.trim() == "exit" {
                     println!("Bye!");
                     break;
@@ -59,6 +76,7 @@ fn main() {
                         }
                     },
                     Err(e) => {
+                        proc_status_flag = false;
                         println!("{:?}", e);
                     }
                 }
